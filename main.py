a8d7a51b2a02f54021fd81cd35f5a343a41747d8
lucashadfield <lucas.hadfield@gmail.com>
2022-12-26 07:13:50 +1100

    add max time

--- main.py
+++ main.py
@@ -22,9 +22,7 @@ def get_service(credentials_path: Path, token_path: Path):
         if credentials and credentials.expired and credentials.refresh_token:
             credentials.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                str(credentials_path), SCOPES
-            )
+            flow = InstalledAppFlow.from_client_secrets_file(str(credentials_path), SCOPES)
             credentials = flow.run_local_server(port=0)
         with token_path.open('wb') as token:
             pickle.dump(credentials, token)
@@ -35,9 +33,7 @@ def get_service(credentials_path: Path, token_path: Path):
 def get_calendar_events(service, timezone: str, calendar_id: str):
     start_date = datetime.date.today()
 
-    start = datetime.datetime.combine(
-        start_date, datetime.time(0), pytz.timezone(timezone)
-    ).astimezone(pytz.UTC)
+    start = datetime.datetime.combine(start_date, datetime.time(0), pytz.timezone(timezone)).astimezone(pytz.UTC)
     end = start + datetime.timedelta(days=1)
 
     time_min = start.replace(tzinfo=None).isoformat() + 'Z'
@@ -57,19 +53,20 @@ def get_calendar_events(service, timezone: str, calendar_id: str):
 
 
 def calculate_trigger_times(
-    events: dict, timezone: str, end_threshold_seconds: int, trigger_offset_seconds: int
+    events: dict, timezone: str, end_threshold_seconds: int, trigger_offset_seconds: int, max_meeting_standing_time: int
 ):
     trigger_times = []
     prev_end_time = None
     for event in events['items']:
         start_time = parse(event['start']['dateTime'])
+        end_time = parse(event['end']['dateTime'])
         if (
             prev_end_time is None
             or (start_time - prev_end_time).total_seconds() >= end_threshold_seconds
+            or (end_time - start_time) <= max_meeting_standing_time
         ):
             trigger_times.append(
-                start_time.astimezone(pytz.timezone(timezone))
-                - datetime.timedelta(seconds=trigger_offset_seconds)
+                start_time.astimezone(pytz.timezone(timezone)) - datetime.timedelta(seconds=trigger_offset_seconds)
             )
         prev_end_time = parse(event['end']['dateTime'])
     return trigger_times
@@ -95,14 +92,7 @@ def append_cron_jobs(trigger_times, cron_backup_path, cron_delimiter, trigger_sc
                 break
 
     with open(cron_backup_path, 'w') as f:
-        cron_append = (
-            cron[:i]
-            + [cron_delimiter]
-            + [
-                f'{datetime_to_cron(t)} {trigger_script}\n'
-                for t in trigger_times
-            ]
-        )
+        cron_append = cron[:i] + [cron_delimiter] + [f'{datetime_to_cron(t)} {trigger_script}\n' for t in trigger_times]
         f.writelines(cron_append)
 
 
@@ -113,6 +103,7 @@ def main():
     timezone = 'Australia/Sydney'  # timezone of the machine running cron
     calendar_id = 'Calendar Name'  # name of the calendar to fetch (could be email address if it's a shared calendar)
     end_threshold_seconds = 600  # if previous meeting ends within 600 seconds of this one, don't raise
+    max_meeting_standing_time = 3600  # don't raise if meeting is longer than this
     trigger_offset_seconds = 60  # how many seconds before the start of the meeting should the desk raise
     cron_delimiter = '# Desk Actions\n'  # replace everything below this line in cron with the updated jobs
     trigger_script = 'python /home/user/autodesk/trigger.py'  # cron command
@@ -125,7 +116,9 @@ def main():
     events = get_calendar_events(service, timezone, calendar_id)
 
     # work out when we should trigger based on those event times
-    trigger_times = calculate_trigger_times(events, timezone, end_threshold_seconds, trigger_offset_seconds)
+    trigger_times = calculate_trigger_times(
+        events, timezone, end_threshold_seconds, trigger_offset_seconds, max_meeting_standing_time
+    )
 
     # backup cron to file
     backup_cron(cron_backup_path)
